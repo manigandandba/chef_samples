84359dab978f77cd3fc6349838031c0ea3f2514f
root <root@centos-7-1.c.chef-learning-212617.internal>
2018-08-08 05:21:53 +0000

    Dynamic variables added

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,3 +4,5 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 include_recipe 'apache::setup'
+include_recipe 'apache::service'
+include_recipe 'apache::webcontent'
--- cookbooks/workstation/recipes/default.rb
+++ cookbooks/workstation/recipes/default.rb
@@ -3,3 +3,20 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'tree' do
+ action :install
+end
+
+package 'ntp' do
+ action :install
+end
+
+file '/etc/motd' do
+ content "Managed by Chef
+ Hostname: #{node['hostname']}
+ FQDN : #{node['fqdn ']}
+ IPAddress : #{node['ipaddress']}
+ CPU MHz : #{node['cpu']['mhz']}
+ Owner : Mani"
+end
