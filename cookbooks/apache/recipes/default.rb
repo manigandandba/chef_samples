26b5aea722653c5203d0e8e0ee6f1f36b7857d5a
root <root@centos-7-1.c.chef-learning-212617.internal>
2018-08-08 08:46:36 +0000

    Unix groups and users add/modify

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -6,5 +6,6 @@
 #include_recipe 'apache::setup'
 #include_recipe 'apache::service'
 #include_recipe 'apache::webcontent'
-include_recipe 'apache::webtemplate'
-include_recipe 'apache::staticfiledeploy'
+#include_recipe 'apache::webtemplate'
+#include_recipe 'apache::staticfiledeploy'
+include_recipe 'apache::grp_user'
